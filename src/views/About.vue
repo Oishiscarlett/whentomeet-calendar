8e9bf60423a380691193ae752d044ea8f57a6ab7
oishi <oishi4869@163.com>
2020-11-03 17:31:37 +0800

    日历组件v1.0.0

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Calendar/>
   </div>
 </template>
+
+<script>
+import Calendar from '@/components/FullCalendar'
+
+export default {
+  name: 'About',
+  components: {
+    Calendar
+  }
+}
+</script>
